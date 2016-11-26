0b0be3f2218df8bbfa277742caefb15f1e48afd9
Julian Schutsch <julian.schutsch@sluonics.com>
2016-11-26 12:52:00 +0100

    serialize text

--- src/requirements/content_text.cpp
+++ src/requirements/content_text.cpp
@@ -1,14 +1,23 @@
 #include "requirements/content_text.hpp"
 
+#include <istream>
+#include <ostream>
+#include <iterator>
+
+#include "requirements/icontentvisitor.hpp"
+
 namespace requirements {
 
 void Content_Text::visit(IContentVisitor& visitor) {
+  visitor.handleContent(*this);
 }
 
 void Content_Text::serialize(std::ostream& dest) {
+  dest<<content;
 }
 
 void Content_Text::deserialize(std::istream& source) {
+  content = std::string(std::istream_iterator<char>(source), {});
 }
 
 }
