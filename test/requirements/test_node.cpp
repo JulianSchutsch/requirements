a2d36c6d3ac21f473e2682c6ee232bf365e7e1ab
Julian Schutsch <julian.schutsch@sluonics.com>
2016-12-30 19:45:10 +0100

    delete test

--- test/requirements/test_node.cpp
+++ test/requirements/test_node.cpp
@@ -7,6 +7,7 @@
 #include "test/nodeset.hpp"
 
 static const char* testContent1 = "Some random content\nNo spefic stuff";
+static const char* testContent2 = "Some other content";
 
 TEST(Node, Create) {
   test::UniqueFolder folder;
@@ -21,9 +22,69 @@ TEST(Node, Create) {
     ::requirements::storage::Text storage(folder.getName(), false);
     auto& collection = storage.getNodeCollection();
     auto nodes = test::extractNodes(collection);
-    ASSERT_TRUE(nodes.size()==1);
+    ASSERT_EQ(nodes.size(), 1);
     auto node = nodes[0];
-    ASSERT_TRUE(node->getId()==nodeId);
-    ASSERT_TRUE(node->getParent()==collection.getRootNode());
+    ASSERT_EQ(node->getId(), nodeId);
+    ASSERT_EQ(node->getParent(), collection.getRootNode());
+    ASSERT_EQ(node->getContent(), testContent1);
+    ASSERT_EQ(node->getAnnotations(), "");
+  }
+}
+
+TEST(Node, SetParent) {
+  test::UniqueFolder folder;
+  ::requirements::Id parentId;
+  ::requirements::Id childId;
+  {
+    ::requirements::storage::Text storage(folder.getName(), true);
+    auto& collection = storage.getNodeCollection();
+    auto parentNode = collection.createNode(testContent1);
+    auto childNode = collection.createNode(testContent2);
+    parentId = parentNode->getId();
+    childId = childNode->getId();
+    childNode->setParent(parentNode);
+  }
+  {
+    ::requirements::storage::Text storage(folder.getName(), false);
+    auto& collection = storage.getNodeCollection();
+    auto rootNode = collection.getRootNode();
+    auto topLevel = rootNode->getChildren();
+    ASSERT_EQ(topLevel.size(), 1);
+    auto parentNode = topLevel.front();
+    ASSERT_EQ(parentNode->getId(), parentId);
+    ASSERT_EQ(parentNode->getContent(), testContent1);
+    ASSERT_EQ(parentNode->getAnnotations(), "");
+    auto children = parentNode->getChildren();
+    ASSERT_EQ(children.size(), 1);
+    auto childNode = children.front();
+    ASSERT_EQ(childNode->getId(), childId);
+    ASSERT_EQ(childNode->getContent(), testContent2);
+    ASSERT_EQ(childNode->getAnnotations(), "");
+    ASSERT_EQ(childNode->getParent(), parentNode);
+  }
+}
+
+TEST(Node, Delete) {
+  test::UniqueFolder folder;
+  {
+    ::requirements::storage::Text storage(folder.getName(), true);
+    auto& collection = storage.getNodeCollection();
+    auto node = collection.createNode(testContent1);
+  }
+  {
+    ::requirements::storage::Text storage(folder.getName(), true);
+    auto& collection = storage.getNodeCollection();
+    auto nodes = test::extractNodes(collection);
+    ASSERT_EQ(nodes.size(), 1);
+    collection.deleteNode(nodes[0]);
+  }
+  {
+    ::requirements::storage::Text storage(folder.getName(), false);
+    auto& collection = storage.getNodeCollection();
+    auto nodes = test::extractNodes(collection);
+    ASSERT_EQ(nodes.size(), 0);
+    auto rootNode = collection.getRootNode();
+    auto topLevel = rootNode->getChildren();
+    ASSERT_EQ(topLevel.size(), 0);
   }
 }
