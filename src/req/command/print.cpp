22eb3d28530c315ff0c6e1c662a2512e486d4d9a
Julian Schutsch <julian.schutsch@sluonics.com>
2017-01-10 21:43:00 +0100

    simple latex requirements document generator

--- src/req/command/print.cpp
+++ src/req/command/print.cpp
@@ -1,13 +1,15 @@
 #include "req/command/print.hpp"
 
 #include <iostream>
+#include <fstream>
 
 #include "requirements/storage/text.hpp"
 
 #include "annotations/parser.hpp"
 
 #include "req/status.hpp"
-#include "req/exception.hpp"
+
+#include "generators/latex/requirements.hpp"
 
 namespace req {
   namespace command {
@@ -15,17 +17,23 @@ namespace req {
     void processCommand_print(Status& status, const std::vector<std::string>& parameters) {
       ::requirements::storage::Text storage(status.folder, false);
       ::annotations::ParserResult parsed;
-      if(::annotations::parse(storage, parsed)) {
-        std::cout<<"No issues found during parsing"<<std::endl;
+      if(!::annotations::parse(storage, parsed)) {
+        std::cout<<"Some issues found during parsing"<<std::endl;
+        for(auto node: storage.getNodeCollection()) {
+          auto id = node->getId();
+          if(parsed.errors.has(id)) {
+            std::cout<<::requirements::id_to_string(id)<<" -> "<<parsed.errors.get(id)<<std::endl;
+          }
+        }
         return;
       }
-      std::cout<<"Some issues found during parsing"<<std::endl;
-      for(auto node: storage.getNodeCollection()) {
-        auto id = node->getId();
-        if(parsed.errors.has(id)) {
-          std::cout<<::requirements::id_to_string(id)<<" -> "<<parsed.errors.get(id)<<std::endl;
-        }
+      std::fstream file(status.folder+"/latex/requirements.tex", std::fstream::out);
+      if(!file) {
+        std::cout<<"Could not open target file"<<std::endl;
+        return;
       }
+      ::generators::latex::printRequirements(parsed.sections, parsed.requirements, file);
+      std::cout<<"Generated requirements document in requirements "<<status.folder<<"/latex/requirements.tex"<<std::endl;
     }
   }
 }
